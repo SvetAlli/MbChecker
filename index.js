9ee957a4c3499ad269242a829e6a8b8dc13ded9d
SvetAlli <SvetAlli@email.com>
2023-02-03 14:16:40 +0100

    Add support for hq and other dc listings

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
-import { serialize, deserialize } from "bson";
-import WebSocket from "ws";
-import fetch from "node-fetch";
+import { serialize, deserialize } from 'bson';
+import WebSocket from 'ws';
+import fetch from 'node-fetch';
+
+
+const WANTED_RATIO = 2;
+const WANTED_MINIMUM_NET_GAIN = 50000;
 
 const worlds = new Map();
 worlds.set(33, 'Twintania');
@@ -12,39 +16,57 @@ worlds.set(67, 'Shiva');
 worlds.set(403, 'Raiden');
 
 
-const addr = "wss://universalis.app/api/ws";
+const addr = 'wss://universalis.app/api/ws';
 
 const ws = new WebSocket(addr);
 
 
 
-const getInfoFromAlpha = async (item) => {
-    const response = await fetch(`https://universalis.app/api/v2/402/${item}`);
+const getInfoFromAlpha = async (item, hq) => {
+    const response = await fetch(`https://universalis.app/api/v2/402/${item}?hq=${hq}`);
     const json = await response.json();
     return json.listings[0]?.pricePerUnit;
-} 
+}
+
+const getInfoFromLight = async (item, hq) => {
+    const response = await fetch(`https://universalis.app/api/v2/light/${item}?hq=${hq}`);
+    const json = await response.json();
+    return json.listings[0];
+}
 
 
-ws.on("open", () => {
+ws.on('open', () => {
   worlds.forEach((value,key) => {
-    ws.send(serialize({ event: "subscribe", channel: `listings/add{world=${key}}` }));
+    ws.send(serialize({ event: 'subscribe', channel: `listings/add{world=${key}}` }));
   });
-  console.log("Connection opened.");
+  console.log('Connection opened.');
 });
 
-ws.on("close", () => console.log("Connection closed."));
+ws.on('close', () => console.log('Connection closed.'));
 
-ws.on("message", async data => {
+ws.on('message', async data => {
     const message = deserialize(data);
     const [listing, ...rest] = message.listings;
     const { item, world } = message;
-
-    const alphaPrice = await getInfoFromAlpha(item);
+    const alphaPrice = await getInfoFromAlpha(item, listing.hq);
     const ratio =  alphaPrice / listing.pricePerUnit;
     const theoricalNetGain = alphaPrice * listing.quantity - listing.pricePerUnit * listing.quantity
-    if(ratio > 5 & theoricalNetGain > 100000) {
+    if(ratio > WANTED_RATIO & theoricalNetGain > WANTED_MINIMUM_NET_GAIN) {
+        const newInfo = await getInfoFromLight(item, listing.hq);
+        const newRatio =  alphaPrice / newInfo.pricePerUnit;
+        const newTheoricalNetGain = alphaPrice * newInfo.quantity - newInfo.pricePerUnit * newInfo.quantity
         const response = await fetch(`https://xivapi.com/item/${item}`, { mode: 'cors' });
         const json = await response.json();
-        console.log({ name: json.Name_en, world: worlds.get(world), pricePerUnit: listing.pricePerUnit, totalPrice: listing.pricePerUnit * listing.quantity, alphaPrice, ratio: ratio.toFixed(2), theoricalNetGain});
+        console.log(
+            { 
+                name: json.Name_en,
+                world: newInfo.worldName,
+                hq: listing.hq,
+                pricePerUnit: newInfo.pricePerUnit,
+                totalPrice: newInfo.pricePerUnit * newInfo.quantity,
+                alphaPrice,
+                ratio: newRatio.toFixed(2),
+                theoricalNetGain: newTheoricalNetGain
+            });
     }
 });
